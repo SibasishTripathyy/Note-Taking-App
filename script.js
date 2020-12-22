8b077c8cca87b78e11c4adea576db42bf0b542b7
Sibasish Tripathyy <sibasishtripathyy@gmail.com>
2020-12-22 12:18:20 +0530

    Added Base Modal structure (not in action)

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+const titleInp = document.getElementById("basic-addon1");
 const textInp = document.getElementById("floatingTextarea");
 const addButton = document.getElementById("addButton");
 const tBody = document.getElementById("tBody");
 var colCtr = false;
 
 class addItem {
-	constructor(inputText) {
-		this.createNewInput(inputText);
+	constructor(inputText, textInp) {
+		this.createNewInput(inputText, textInp);
 	}
 
-	createNewInput(inputText) {
+	createNewInput(inputText, titleInp) {
 
 		var tr = document.createElement("tr");
 
@@ -23,15 +24,47 @@ class addItem {
 
 		var hText = document.createElement("h5");
 		hText.classList.add("card-title");
-		hText.innerHTML = "Card Title";
+		hText.innerHTML = textInp;
 
 		var para = document.createElement("p");
 		para.classList.add("card-text");
-		para.innerHTML = inputText;
+		para.innerHTML = titleInp;
 
 		var link = document.createElement("a");
 		link.classList.add("btn", "btn-primary");
-		link.innerHTML = "Go Somewhere";
+		link.innerHTML = "View Detailed Info";
+		link.setAttribute("data-toggle", "modal");
+		link.setAttribute("href", "#exampleModal");
+
+		//Modal
+		var modalDiv1 = document.createElement("div");
+		modalDiv1.classList.add("modal fade");
+		modalDiv1.setAttribute("id", "exampleModal");
+		modalDiv1.setAttribute("tabindex", "-1");
+		modalDiv1.setAttribute("role", "dialog");
+		modalDiv1.setAttribute("aria-labelledby", "exampleModalLabel");
+		modalDiv1.setAttribute("aria-hidden", "true");
+
+		var modalDiv2 = document.createElement("div");
+		modalDiv2.classList.add("modal-dialog");
+		modalDiv2.setAttribute("role", "document");
+
+		var modalDiv3 = document.createElement("div");
+		modalDiv3.classList.add("modal-content");
+
+		var modalDiv4 = document.createElement("div");
+		modalDiv4.classList.add("modal-header");
+
+		var modalTitle = document.createElement("h5");
+		modalTitle.classList.add("modal-title");
+		// skipping title id for now.
+		modalTitle.innerHTML = titleInp;
+
+		var modalDiv5 = document.createElement("div");
+		modalDiv5.classList.add("modal-body");
+
+		var modalBody = document.createElement("p");
+		modalBody.innerHTML = inputText;
 
 		if (colCtr == false) {
 			tBody.appendChild(tr);
@@ -42,6 +75,7 @@ class addItem {
 		tBody.lastChild.appendChild(td);
 		td.appendChild(itemOuterDiv);
 		td.appendChild(itemInnerDiv);
+		//append modal here
 		td.appendChild(hText);
 		td.appendChild(para);
 		td.appendChild(link);
@@ -50,8 +84,9 @@ class addItem {
 }
 
 addButton.addEventListener('click', function() {
-	if(textInp.value != "") {
-		new addItem(textInp.value);
+	if(textInp.value != "" && titleInp.value != "") {
+		new addItem(textInp.value, titleInp.value);
 		textInp.value = "";
+		titleInp.value = "";
 	}
 })
